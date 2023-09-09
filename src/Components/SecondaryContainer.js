5294cff6ed4d287940ffe0b49de9af74732bc9fb
MeghanaPG <meghanapg3@gmail.com>
2023-09-09 02:20:34 -0400

    Secondary Container && MovieList && Popular Movies

--- src/Components/SecondaryContainer.js
+++ src/Components/SecondaryContainer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import MovieList from './MovieList'
 
 const SecondaryContainer = () => {
-  return (
-    <div>SecondaryContainer</div>
-  )
-}
+  const movies = useSelector((store) => store.movies);
 
-export default SecondaryContainer
+  return (
+    movies.nowPlayingMovies && (
+     <div className="bg-black">
+      <div className=" -mt-24 pl-12 relative z-20">
+      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Popular"} movies={movies.popularMovies}/>
+      <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Horror Movies"} movies={movies.nowPlayingMovies}/>
+    </div>
+    </div>
+    )
+  );
+};
+
+export default SecondaryContainer;
---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../Utils/constants";
+import { addPopularMovies } from "../Utils/moviesSlice";
+import { useEffect } from "react";
+
+const usePopularMovies = () => {
+  // Fetch data from TMDB API and update store
+  const dispatch = useDispatch();
+
+  // Making API Call
+  const getPopularMovies = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?page=1', 
+    API_OPTIONS
+    );
+    //To convert data into Json readable stream 
+    const json = await data.json();
+    dispatch(addPopularMovies(json.results));
+  };
+
+  //Calling the API call inside useEffect so that we can call API only once 
+  useEffect(() =>{
+    getPopularMovies();
+  }, [])
+}
+
+export default usePopularMovies;
